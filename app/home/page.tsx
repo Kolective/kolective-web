07ff32dac9ac493e118fd1c351b5e8662fed1448
FjrREPO <jossfajar21@gmail.com>
2025-03-02 16:37:13 +0700

    add particle effects for snow animation, update layout background color, and modify package dependencies

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,13 +4,33 @@ import { LinkSoniclabs } from "@/components/link/link-soniclabs";
 import { subtitle } from "@/components/primitives";
 import { cn } from "@/lib/utils";
 import { motion } from "framer-motion";
+import particlesOptions from "@/components/animation/snow.json";
+import { loadFull } from "tsparticles";
+import { ISourceOptions } from "@tsparticles/engine";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useState, useEffect } from "react";
 
 export default function Page() {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
   return (
     <section
       id="home"
       className="inline-flex w-full h-full items-center justify-center flex-grow"
     >
+      {init && (
+        <Particles
+          options={particlesOptions as unknown as ISourceOptions}
+        />
+      )}
       <div className="max-w-lg text-center inline-block z-10">
         <div className="flex flex-col items-center justify-center gap-1">
           <motion.div
