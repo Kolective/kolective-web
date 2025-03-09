6c8cc15e7cadbeea362e791f05cd2b93d1cca0c7
FjrREPO <jossfajar21@gmail.com>
2025-03-09 14:14:11 +0700

    application configuration Kolective

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -2,7 +2,7 @@ import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { sonicBlazeTestnet } from 'viem/chains';
 
 export const config = getDefaultConfig({
-    appName: 'OriginX',
+    appName: 'Kolective',
     projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
     chains: [
       sonicBlazeTestnet
